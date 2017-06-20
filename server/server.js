7441a7d02f01dfd28b934a548dace5387ebfa0d9
Andrew Betts <andrew@trib.tv>
2017-06-20 14:23:23 -0700

    Use my own server, max stream duration

--- server/server.js
+++ server/server.js
@@ -4,37 +4,27 @@ require('dotenv').load();
 
 const path = require('path');
 const express = require('express');
-const SSE = require('sse-node');
 const events = require('events');
 const moods = require('../data/moods.json');
+const SSEChannel = require('./sse.js');
 
 const PORT = process.env.PORT || 3101;
 const MIN_INTERVAL = 300;
 const MAX_INTERVAL = 3000;
 
 const app = express();
-const eventEmitter = new events.EventEmitter();
-let clientsConnected = 0;
+const sse = SSEChannel();
 
 // Serve the static part of the demo
 app.use(express.static('public'));
 
-// Process SSE requests
-app.get('/stream', (req, res) => {
-  const client = SSE(req, res, {ping: 5000})
-	clientsConnected++;
-  eventEmitter.on('moodChange', newMood => {
-		client.send("Telling "+clientsConnected+" "+((clientsConnected===1)?"person":"people")+" that I am now feeling "+newMood.toLowerCase(), "moodChange");
-  });
-	client.onClose(() => {
-		clientsConnected--;
-	});
-});
+// Process SSE subscribers
+app.get('/stream', (req, res) => sse.subscribe(req, res));
 
 // Return a 404 if no routes match
 app.use((req, res, next) => {
   res.set('Cache-Control', 'max-age=0; private');
-  res.status(404).render("not-found");
+  res.status(404).end('Not found');
 });
 
 app.listen(PORT, () => {
@@ -43,7 +33,8 @@ app.listen(PORT, () => {
 
 
 function changeMood() {
-	eventEmitter.emit('moodChange',  moods[Math.floor(Math.random() * moods.length)]);
+	const newMood = moods[Math.floor(Math.random() * moods.length)];
+  sse.publish({newMood:newMood, clientCount: sse.getSubscriberCount()}, 'moodChange');
 	setTimeout(changeMood, Math.round(MIN_INTERVAL+((MAX_INTERVAL-MIN_INTERVAL)*Math.random())));
 }
 changeMood();
---
+++ server/sse.js
@@ -0,0 +1,56 @@
+module.exports = function SSEChannel(options) {
+
+	options = Object.assign({}, options, {
+		pingInterval: 3000,
+		maxStreamDuration: 30000,
+		clientRetryInterval: 5000
+	});
+
+	let msgID = 0;
+	const clients = new Set();
+
+	function publish(data, eventName) {
+		const thisID = msgID++;
+		if (typeof data === "object") data = JSON.stringify(data);
+		data = data ? data.split(/[\r\n]+/).map(str => 'data: '+str).join('\n') : '';
+
+		clients.forEach(res => {
+			if (data) res.write("id: " + thisID + "\n");
+			if (eventName) res.write("event: " + eventName + "\n");
+			res.write(data+'\n\n');
+		});
+	}
+
+	function subscribe(req, res) {
+		req.socket.setNoDelay(true);
+		res.writeHead(200, {
+			"Content-Type": "text/event-stream",
+			"Cache-Control": "no-cache",
+			"Connection": "keep-alive"
+		});
+		res.write("retry: " + options.clientRetryInterval + '\n');
+		clients.add(res);
+		setTimeout(() => {
+			if (!res.finished) {
+				unsubscribe(res);
+			}
+		}, options.maxStreamDuration);
+		res.on('close', () => unsubscribe(res));
+	}
+
+	function unsubscribe(res) {
+		console.log("Unsubscribed");
+		clients.delete(res);
+		res.end();
+	}
+
+	if (options.pingInterval) {
+		setInterval(publish, options.pingInterval);
+	}
+
+	return {
+		publish:publish,
+		subscribe: subscribe,
+		getSubscriberCount: () => clients.size
+	};
+};
